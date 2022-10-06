f382e9f5c595c5c1ac37f44d5e1644a328448b70
EstebanMarin <estebanmarin002@gmail.com>
2022-10-06 10:59:56 -0500

    working

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -6,11 +6,17 @@ import Gen.*
 import Prop.forAll
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] =
+    oneOf(
+      const(empty),
+      for 
+        v <- arbitrary[Int]
+        h <- oneOf(const(empty), genHeap)
+      yield insert(v, h)
+    )
   given Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
     val m = if isEmpty(h) then 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
-
